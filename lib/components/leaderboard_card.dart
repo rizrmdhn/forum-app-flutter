73f9e2df909bc29cbe4f2be59c5124564b2e54a8
rizrmdhn <rizrmdhn.work@gmail.com>
2023-09-13 15:49:50 +0800

    add avatar to leaderboard screen

--- lib/components/leaderboard_card.dart
+++ lib/components/leaderboard_card.dart
@@ -23,13 +23,31 @@ class LeaderboardCardState extends State<LeaderboardCard> {
             flex: 1,
             child: Center(
               child: Container(
-                margin: const EdgeInsets.only(bottom: 5, right: 45),
-                child: Text(
-                  widget.user.name.toString(),
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                  ),
+                margin: const EdgeInsets.only(bottom: 5, right: 30),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                            'https://ui-avatars.com/api/?name=${widget.user.name}&length=1&background=random&size=128',
+                            width: 28,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                            widget.user.name.toString(),
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
